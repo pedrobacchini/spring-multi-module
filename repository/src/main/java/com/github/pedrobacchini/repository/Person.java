cd842a998a328d6412fef5ea9e7bed45f327afb5
Pedro Bacchini <pedrobacchini@outlook.com>
2019-07-25 18:19:28 -0300

    Loading initial data.

--- repository/src/main/java/com/github/pedrobacchini/repository/Person.java
+++ repository/src/main/java/com/github/pedrobacchini/repository/Person.java
@@ -1,12 +1,17 @@
 package com.github.pedrobacchini.repository;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-@Entity
 @Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE) //For Hibernate
 public class Person {
     @Id
     private Long id;
--- resource/src/main/java/com/github/pedrobacchini/ResourceApplication.java
+++ resource/src/main/java/com/github/pedrobacchini/ResourceApplication.java
@@ -1,11 +1,27 @@
 package com.github.pedrobacchini;
 
+import com.github.pedrobacchini.repository.Person;
+import com.github.pedrobacchini.repository.PersonRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication(scanBasePackages = "com.github.pedrobacchini")
-public class ResourceApplication {
+public class ResourceApplication implements CommandLineRunner {
+
+    private final PersonRepository personRepository;
+
+    public ResourceApplication(PersonRepository personRepository) { this.personRepository = personRepository; }
 
     public static void main(String[] args) { SpringApplication.run(ResourceApplication.class, args); }
 
+    @Override
+    public void run(String... args) {
+        Person person1 = new Person(1L, "Pedro", "Bacchini");
+        Person person2 = new Person(2L, "Maria", "Silva");
+
+        personRepository.saveAll(Arrays.asList(person1, person2));
+    }
 }
